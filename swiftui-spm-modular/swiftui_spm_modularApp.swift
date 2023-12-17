2084132cda433ee85ce79b3e8728be0d3e803388
CLVargas <cl.vargas@globant.com>
2023-12-17 13:15:47 -0500

    Initial Commit

---
+++ swiftui-spm-modular/swiftui_spm_modularApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_spm_modularApp.swift
+//  swiftui-spm-modular
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_spm_modularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
